e5955d746a3d5f4837658c0bbc9d56c38811574c
anmelnik <melnikvanastasia@gmail.com>
2021-01-17 17:49:03 +0200

    weather applications

---
+++ weather/src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Form from "./components/form";
+import Info from "./components/info";
+import Weather from "./components/weatherInfo";
+
+const API_KEY = "10512a16fb911bbe66cb48fbd6917e95";
+
+class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined,
+  };
+
+  gettingWeaher = async (e) => {
+    e.preventDefault();
+    let city = e.target.elements.city.value;
+
+    if (city) {
+      const api_url = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_url.json();
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunset: sunset_date,
+        error: undefined,
+      });
+    } else {
+      this.setState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: "Enter city names",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-5 info">
+                <Info />
+              </div>
+              <div className="col-sm-7 form">
+                <Form weatherMethod={this.gettingWeaher} />
+                <Weather
+                  temp={this.state.temp}
+                  city={this.state.city}
+                  country={this.state.country}
+                  pressure={this.state.pressure}
+                  sunset={this.state.sunset}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
